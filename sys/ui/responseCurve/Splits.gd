203ca3fb0fe71469f69f13f50702a08ea5bdc720
nobuyuki_nyuu <nobuyuki.nyuu@gmail.com>
2020-05-02 11:43:10 -0500

    Response curve UI work

--- sys/ui/responseCurve/Splits.gd
+++ sys/ui/responseCurve/Splits.gd
@@ -1,7 +1,10 @@
 extends TextureRect
 
+var mask = ImageTexture.new()
+
 func _ready():
 	
+	$"0".connect("toggled", self, "_on_Split_toggled", ["0"])
 	for i in range(1,32):
 		var p = $"0".duplicate()
 		p.name = str(i)
@@ -18,6 +21,26 @@ func _ready():
 		if i == 31:  p.hint_tooltip = "> %s-%s | (No effect)" % [v, v+3]
 	pass
 
+	#Pass in a texture to the VU split indicator
+	var img:Image = Image.new()
+	img.create(32,1,false,Image.FORMAT_RGB8)
+	mask.create_from_image(img,0)
+	assert (mask!=null)
+	assert (img!=null)
+	
+	owner.get_node("VU/Overlay").material.set_shader_param("mask", mask)
+
 func _on_Split_toggled(pressed, whomst):
 #	get_node(whomst)
+	
+	var column=int(whomst)
+	var tex = mask
+
+	
+	if tex !=null:
+		var img:Image = tex.get_data()
+		img.lock()
+		img.set_pixel(column,0, Color(255<<24) if pressed else Color(0) )
+		img.unlock()
+		mask.create_from_image(img,0)
 	pass
--- sys/ui/responseCurve/VUMeter.gd
+++ sys/ui/responseCurve/VUMeter.gd
@@ -4,12 +4,16 @@ var tbl = []
 const full16 = preload("res://gfx/ui/vu/full16.png")
 const indicator = preload("res://gfx/ui/vu/indicator16.png")
 
-var elementWidth = 10
+var elementWidth = 10  #How wide is a column?
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int,1,255) var maxValue = 100
+export(bool) var linLog = false
 
+var cursor = preload("res://gfx/ui/vu/cursor.png").get_data()
+var numbers = preload("res://gfx/fonts/small_numerics_thin.png").get_data()
+var cursor_img = Image.new()
+var cursor_texture=ImageTexture.new()
+var custom_texture=ImageTexture.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,12 +23,32 @@ func _ready():
 	for i in 128:
 		tbl.append(0)
 
+	cursor_img.create(24,16,false,Image.FORMAT_RGBA8)
+	var tex = ImageTexture.new()
+
+	cursor_img.lock()
+	cursor_img.blit_rect(cursor,Rect2(Vector2.ZERO, cursor.get_size()),Vector2.ZERO)
+	cursor_img.unlock()
+#	set_cursor("01.9")
+	cursor_texture.create_from_image(cursor_img,0)
+	custom_texture.create_from_image(cursor_img,0)
+#	cursor_texture = tex
+
+
 
 func _gui_input(event):
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(BUTTON_LEFT):
 		var arpos = clamp(int(lerp(0, 127, get_local_mouse_position().x / float(rect_size.x))) , 0, 127)
 		var val = clamp(lerp(100,0, get_local_mouse_position().y / float(rect_size.y)) , 0, 100)
 #		prints("arpos",arpos,"val",val)
+
+		var vol = maxValue * (val/100.0)
+		if linLog:  vol = (val/float(maxValue)) * (val/float(maxValue)) * maxValue
+		
+		set_cursor(String(vol))
+		Input.set_custom_mouse_cursor(cursor_texture,0,Vector2(0,0)) #Temporarily blank to update
+		Input.set_custom_mouse_cursor(custom_texture,0,Vector2(0,0))
+
 
 		tbl[arpos] = val
 
@@ -41,6 +65,8 @@ func _gui_input(event):
 			tbl[i] = val
 
 		update()
+	else:
+		Input.set_custom_mouse_cursor(null)
 
 func _draw():
 	
@@ -52,3 +78,25 @@ func _draw():
 		draw_texture(indicator,pos)
 #		if i == 310:  prints("drawrect", i, ":", pos, sz)
 
+
+
+func set_cursor(volume:String):
+	cursor_img.lock()
+
+	for i in range(1,4):
+		cursor_img.blit_rect(numbers,Rect2(4,0,4,8), Vector2(i*4 +8,8))
+	
+	for i in min(4, String(int(volume)).length()):
+		var n = int(volume.ord_at(i))
+		
+		var pos = Vector2(4,0)
+		if n == 46:
+			pos = Vector2.ZERO
+		elif (n>=48 and n<58):
+			pos = Vector2((n-48)*4 + 8, 0)
+		cursor_img.blit_rect(numbers,Rect2(pos, Vector2(4,8)),Vector2(i*4 +8,8))
+	
+#	if volume.begins_with("100"):  cursor_img.blit_rect(numbers,Rect2(4,0,4,8), Vector2(12,8))
+	
+	cursor_img.unlock()
+	custom_texture.create_from_image(cursor_img,0)
